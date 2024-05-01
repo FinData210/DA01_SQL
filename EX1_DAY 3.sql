614914e0e657e442187e469aa43ee2cb95ccaad1
FinData210 <168623386+FinData210@users.noreply.github.com>
2024-05-01 15:09:06 +0700

    Update EX1_DAY 3.sql

--- EX1_DAY 3.sql
+++ EX1_DAY 3.sql
@@ -1,3 +1,4 @@
 -- bài tập 1: Tìm trường thông tin NAME trong bảng CITY với Population lớn hơn 120000
-select NAME, POPULATION from CITY 
-where POPULATION > 120000
+select NAME from CITY 
+Where (COUNTRYCODE = 'USA')
+and (POPULATION >120000)
